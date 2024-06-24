8be319b72069062727de2bd4f571eca199e639ce
maharaniputrie <maharani.putriefendi@gmail.com>
2024-06-24 14:53:23 +0700

    ngubah layout dikit

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -1,28 +1,39 @@
 "use client";
 
+
 import { useState, useContext, useRef } from "react";
 import { financeContext } from "@/lib/store/finance-context";
 
+
 import { v4 as uuidv4 } from "uuid";
+
 
 import Modal from "@/components/Modal";
 
+
 import { toast } from "react-toastify";
+
 
 function AddExpensesModal({ show, onClose }) {
   const [expenseAmount, setExpenseAmount] = useState("");
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [paymentMethod, setPaymentMethod] = useState("");
   const [showAddExpense, setShowAddExpense] = useState(false);
+ 
+
 
   const { expenses, addExpenseItem, addCategory } = useContext(financeContext);
 
+
   const titleRef = useRef();
   const colorRef = useRef();
+
 
   const addExpenseItemHandler = async () => {
     const expense = expenses.find((e) => {
       return e.id === selectedCategory;
     });
+
 
     const newExpense = {
       color: expense.color,
@@ -34,16 +45,20 @@ function AddExpensesModal({ show, onClose }) {
           amount: +expenseAmount,
           createdAt: new Date(),
           id: uuidv4(),
+          paymentMethod,
         },
       ],
     };
 
+
     try {
       await addExpenseItem(selectedCategory, newExpense);
+
 
       console.log(newExpense);
       setExpenseAmount("");
       setSelectedCategory(null);
+      setPaymentMethod("");
       onClose();
       toast.success("Expense Item Added!");
     } catch (error) {
@@ -52,9 +67,11 @@ function AddExpensesModal({ show, onClose }) {
     }
   };
 
+
   const addCategoryHandler = async () => {
     const title = titleRef.current.value;
     const color = colorRef.current.value;
+
 
     try {
       await addCategory({ title, color, total: 0 });
@@ -66,11 +83,13 @@ function AddExpensesModal({ show, onClose }) {
     }
   };
 
+
   return (
     <Modal show={show} onClose={onClose}>
-      <div className="flex flex-col gap-4">
-        <label>Enter an amount..</label>
-        <input
+      <h2 style={{ fontSize: "2rem", fontWeight: "bold", textAlign: "center" }}> EXPENSES </h2>
+      <div className="input-group" >
+        <label>Enter an amount : </label>
+        <input className="inputan"
           type="number"
           min={0.01}
           step={0.01}
@@ -80,13 +99,30 @@ function AddExpensesModal({ show, onClose }) {
             setExpenseAmount(e.target.value);
           }}
         />
-      </div>
+<label>
+  Select Payment Method :
+</label>
+<select
+  value={paymentMethod}
+  onChange={(e) => setPaymentMethod(e.target.value)}
+  className="inputan"
+>
+  <option value="">Select Payment method</option>
+  <option value="cash" className="text-gray-700">Cash</option>
+  <option value="credit card" className="text-gray-700">Credit Card</option>
+  <option value="debit card" className="text-gray-700">Debit Card</option>
+  <option value="ShopeePay" className="text-gray-700">ShopeePay</option>
+  <option value="GoPay" className="text-gray-700">GoPay</option>
+  <option value="other" className="text-gray-700">Other</option>
+</select>
+</div>
+
 
       {/* Expense Categories */}
       {expenseAmount > 0 && (
         <div className="flex flex-col gap-4 mt-6">
           <div className="flex items-center justify-between">
-            <h3 className="text-2xl capitalize">Select expense category</h3>
+            <label>Select expense category</label>
             <button
               onClick={() => {
                 setShowAddExpense(true);
@@ -97,9 +133,11 @@ function AddExpensesModal({ show, onClose }) {
             </button>
           </div>
 
+
           {showAddExpense && (
             <div className="flex items-center justify-between">
               <input type="text" placeholder="Enter Title" ref={titleRef} />
+
 
               <label>Pick Color</label>
               <input type="color" className="w-24 h-10" ref={colorRef} />
@@ -119,6 +157,7 @@ function AddExpensesModal({ show, onClose }) {
               </button>
             </div>
           )}
+
 
           {expenses.map((expense) => {
             return (
@@ -152,6 +191,7 @@ function AddExpensesModal({ show, onClose }) {
         </div>
       )}
 
+
       {expenseAmount > 0 && selectedCategory && (
         <div className="mt-6">
           <button className="btn btn-primary" onClick={addExpenseItemHandler}>
@@ -162,5 +202,6 @@ function AddExpensesModal({ show, onClose }) {
     </Modal>
   );
 }
+
 
 export default AddExpensesModal;
