8137cf5e2d6b49b10a118be5d400566c2f4e1986
Titus Patrick <tituspat0112@gmail.com>
2024-06-24 19:47:05 +0700

    feat: instance and run

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
 provider "aws" {
-  region = "ap-southeast-1"  # Ganti dengan region AWS yang Anda gunakan
-}
-
-# Membuat VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Membuat Subnet
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1a"  # Ganti dengan availability zone yang Anda gunakan
+  region = "ap-southeast-1"
 }
 
 # Membuat Security Group
@@ -33,31 +21,22 @@ resource "aws_security_group" "allow_port_3000" {
   }
 }
 
-# Menggunakan template untuk user data
-data "template_file" "userdata" {
-  template = file("${path.module}/userdata.tpl")
-
-  vars = {
-    dockerhub_username = var.dockerhub_username
-    dockerhub_password = var.dockerhub_password
-  }
-}
-
-# Membuat Instance EC2
-resource "aws_instance" "app_server" {
-  ami                    = "ami-003c463c8207b4dfa"  # Ganti dengan AMI yang sesuai
-  instance_type          = "t2.micro"
+resource "aws_instance" "app" {
+  ami           = "ami-003c463c8207b4dfa"  # AMI untuk Amazon Linux 2
+  instance_type = "t2.micro"
   subnet_id              = aws_subnet.main.id
   security_groups        = [aws_security_group.allow_port_3000.name]
   associate_public_ip_address = true
 
-  user_data = data.template_file.userdata.rendered
-
   tags = {
-    Name = "AppServer"
+    Name = "ExpressJS-EC2"
   }
+
+  key_name = "spendy-2"  # Ganti dengan nama kunci SSH Anda
+
 }
 
-output "instance_public_ip" {
-  value = aws_instance.app_server.public_ip
+
+output "output" {
+    public_ip = aws_instance.app.public_ip
 }
--- variable.tf
+++ variable.tf
@@ -1,15 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to deploy in"
-  default     = "ap-southeast-1"
-}
-
-variable "dockerhub_username" {
-  description = "DockerHub Username"
-  type        = string
-}
-
-variable "dockerhub_password" {
-  description = "DockerHub Password"
-  type        = string
-  sensitive   = true
+variable "ec2_name" {
+  type = string
 }
