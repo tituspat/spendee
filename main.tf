bfb85f877801052d013b52e311f580f1fa08df6f
Titus Patrick <tituspat0112@gmail.com>
2024-06-24 23:11:34 +0700

    fix: terraform

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Membuat Subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-southeast-1a"
+}
+
 # Membuat Security Group
 resource "aws_security_group" "allow_port_3000" {
   vpc_id = aws_vpc.main.id
