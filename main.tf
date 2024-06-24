def3c1cf8489e182950d6aac12fe94ee57cf11ba
Titus Patrick <tituspat0112@gmail.com>
2024-06-24 22:56:46 +0700

    fix: terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+# Membuat VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 # Membuat Security Group
 resource "aws_security_group" "allow_port_3000" {
   vpc_id = aws_vpc.main.id
