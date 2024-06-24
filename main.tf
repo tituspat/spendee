c7a159a0cd5e16959500c1cf1377eabc2ed864c2
Titus Patrick <tituspat0112@gmail.com>
2024-06-24 18:41:26 +0700

    fix

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1"  # Ganti dengan availability zone yang Anda gunakan
+  availability_zone = "ap-southeast-1a"  # Ganti dengan availability zone yang Anda gunakan
 }
 
 # Membuat Security Group
