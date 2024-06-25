d61f56285a8f896fa299dc398a287e402079bd7d
Titus Patrick <tituspat0112@gmail.com>
2024-06-25 17:31:55 +0700

    h

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_eip" "elastic_ip" {
 
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.public_instance.id
-  allocation_id = aws_eip.elastic_ip.id
+  allocation_id = "eipassoc-02edc7d4d862d375d"
 }
 
 output "instance_ip" {
