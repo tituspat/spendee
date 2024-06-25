a14fa68a0e5cbc51077b252625fd3f18dbcc8f22
Titus Patrick <tituspat0112@gmail.com>
2024-06-25 16:11:54 +0700

    h

--- main.tf
+++ main.tf
@@ -1,49 +1,38 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Membuat VPC
-data "aws_vpc" "main" {
-  id = "vpc-04e356bf9b40a9111"
-}
-
-# Membuat Subnet
-data "aws_subnet" "main" {
-  id = "subnet-0f1d0c19af3a68b6b" 
-}
-
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  rsa_bits = 4096
 }
 
 variable "key_name" {
-  description = "key name for ssh access"
-  default     = "spendy-2"
+  description = "terraform-key"
+  default     = "terraform-key"  
 }
 
 resource "aws_key_pair" "service_key_pair" {
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-# Storing the private key locally
 resource "local_file" "private_key" {
-  content  = tls_private_key.rsa_4096.private_key_pem
-  filename = "spendy-2"
+  content = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
 }
 
 resource "aws_security_group" "allow_http_ssh" {
   description = "Allow HTTP and SSH inbound traffic"
-  # Add your security group configuration here
-
-  vpc_id = data.aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 22
@@ -51,18 +40,6 @@ resource "aws_security_group" "allow_http_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Creating a security group that allows port 3000 inbound traffic
-resource "aws_security_group" "allow_port_3000" {
-  vpc_id = data.aws_vpc.main.id
 
   ingress {
     from_port   = 3000
@@ -72,30 +49,24 @@ resource "aws_security_group" "allow_port_3000" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
 
-# Membuat Instance EC2
-resource "aws_instance" "app" {
-  ami                    = "ami-003c463c8207b4dfa"  # AMI untuk Amazon Linux 2
-  instance_type          = "t2.micro"
-  subnet_id              = data.aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.allow_port_3000.id, aws_security_group.allow_http_ssh.id]
-  associate_public_ip_address = true
-
-  key_name                = aws_key_pair.service_key_pair.key_name
+resource "aws_instance" "public_instance" {
+  ami = "ami-003c463c8207b4dfa"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.service_key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
 
   tags = {
-    Name = "ExpressJS-EC2"
+    Name = "terraform-aws"
   }
-
-  depends_on = [aws_security_group.allow_port_3000, aws_security_group.allow_http_ssh]
 }
 
 output "instance_ip" {
-    value = aws_instance.app.public_ip
+  value = aws_instance.public_instance.public_ip
 }
